2c249fe13fcd121f7c238a8925c8f1291f03255f
urielgutierrezn <uriel.gutierrenz@gmail.com>
2025-12-24 02:57:29 -0700

    Actualizacion clas user

--- app.py
+++ app.py
@@ -59,9 +59,9 @@ os.makedirs(TMP_DIR, exist_ok=True)
 # OBSERVACIÓN: MODELO DE USUARIO
 # ================================================================
 class User(UserMixin, db.Model):
-    id = db.Column(int, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     is_active = db.Column(db.Boolean, default=True) 
     is_admin = db.Column(db.Boolean, default=False)
 
